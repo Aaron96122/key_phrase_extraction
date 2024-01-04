7d9d200723d97154379e8c23b1f5a07c3a0c6f48
Aaron96122 <98258759+Aaron96122@users.noreply.github.com>
2024-01-04 21:14:28 +0800

    Update run_text.sh

--- run_text.sh
+++ run_text.sh
@@ -1,13 +1,5 @@
-#!/bin/sh
-#SBATCH -A uppmax2020-2-2
-#SBATCH -p core -n 4
-#SBATCH -t 03-00:00:00 
-#SBATCH -M snowy
-#SBATCH -J project
-#SBATCH --gres=gpu:1
-
-python3.8 run.py --text /home/yaruwu/keyphrase/Test/232.abstr \
-              --StanfordCoreNLP_path /home/yaruwu/keyphrase/stanford-corenlp-full-2018-10-05/ \
-              --model_path /home/yaruwu/keyphrase/bert-base-uncased \
+python3.8 run.py --text THE_PATH_TO_THE_.TXT_FILE_OR_THE_STRING_OF_TEXT \
+              --StanfordCoreNLP_path ./stanford-corenlp-full-2018-10-05/ \
+              --model_path ./bert-base-uncased \
               --beta 1 \
               --number 15 \
